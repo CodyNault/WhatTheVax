57329021d80e0b79345a14cf0994d5a26b190fee
Joe <jecolasurdo@gmail.com>
2021-02-12 14:47:48 -0800

    WIP: draft of county prioritization done

--- scrape.py
+++ scrape.py
@@ -66,74 +66,69 @@ search_engines = [
 
 
 def main():
-    with open('county_list.csv', newline='') as f:
-        engine_times = dict()
-        r = csv.reader(f, delimiter=',')
-        for row in r:
-            # use these instances to access files (may contain underscores)
-            county, state = row[0], row[1]
+    engine_times = dict()
+    for (county, state) in get_prioritized_county_list():
+        # use these values for search queries and formatted output
+        county_clean, state_clean = replace_underscores(
+            county), replace_underscores(state)
 
-            # use these values for search queries and formatted output
-            county_clean, state_clean = replace_underscores(
-                county), replace_underscores(state)
+        prush("{}, {}...".format(county_clean, state_clean))
 
-            prush("{}, {}...".format(county_clean, state_clean))
+        time_since_last_use = 0
+        engine_name = ""
+        while True:
+            # This does basically constitute a busy loop if all engines are
+            # in a cooldown period, but since this is single threaded, I'm
+            # not too concerned.
+            engine = random.choice(search_engines)()
+            engine_name = engine.__class__.__name__
+            if not engine_name in engine_times:
+                break
+            time_since_last_use = (
+                datetime.now() - engine_times[engine_name]).total_seconds()
+            if time_since_last_use >= ENGINE_MIN_COOLDOWN_SECS:
+                break
 
-            time_since_last_use = 0
-            engine_name = ""
-            while True:
-                # This does basically constitute a busy loop if all engines are
-                # in a cooldown period, but since this is single threaded, I'm
-                # not too concerned.
-                engine = random.choice(search_engines)()
-                engine_name = engine.__class__.__name__
-                if not engine_name in engine_times:
-                    break
-                time_since_last_use = (
-                    datetime.now() - engine_times[engine_name]).total_seconds()
-                if time_since_last_use >= ENGINE_MIN_COOLDOWN_SECS:
-                    break
+        engine.set_headers({'User-Agent': get_random_user_agent()})
+        subject = PREFERRED_SEARCH_TEMPLATE.format(
+            county_clean, state_clean)
+        search_results = engine.search(subject, pages=SEARCH_PAGES).links()
+        engine_times[engine_name] = datetime.now()
 
-            engine.set_headers({'User-Agent': get_random_user_agent()})
-            subject = PREFERRED_SEARCH_TEMPLATE.format(
+        if len(search_results) == 0:
+            subject = ALTERNATE_SEARCH_TEMPLATE.format(
                 county_clean, state_clean)
-            search_results = engine.search(subject, pages=SEARCH_PAGES).links()
-            engine_times[engine_name] = datetime.now()
 
-            if len(search_results) == 0:
-                subject = ALTERNATE_SEARCH_TEMPLATE.format(
-                    county_clean, state_clean)
+            # Random-uniform wait period between successive calls to same
+            # engine adds some delay and jitter to the calls, making it
+            # just slightly harder to get rate-limited.
+            time.sleep(random.uniform(
+                ALTERNATE_SEARCH_MIN_WAIT_SECS, ALTERNATE_SEARCH_MAX_WAIT_SECS))
 
-                # Random-uniform wait period between successive calls to same
-                # engine adds some delay and jitter to the calls, making it
-                # just slightly harder to get rate-limited.
-                time.sleep(random.uniform(
-                    ALTERNATE_SEARCH_MIN_WAIT_SECS, ALTERNATE_SEARCH_MAX_WAIT_SECS))
+            search_results = engine.search(
+                subject, pages=SEARCH_PAGES).links()
 
-                search_results = engine.search(
-                    subject, pages=SEARCH_PAGES).links()
+        title = fmt_title(engine_name, subject)
+        access_time = fmt_access_time()
 
-            title = fmt_title(engine_name, subject)
-            access_time = fmt_access_time()
+        markdown = ""
+        with open(state + "/" + county + ".md", "r") as county_file:
+            markdown = county_file.read()
 
-            markdown = ""
-            with open(state + "/" + county + ".md", "r") as county_file:
-                markdown = county_file.read()
+        if len(search_results) == 0 or search_results[0] in markdown:
+            continue
 
-            if len(search_results) == 0 or search_results[0] in markdown:
-                continue
+        best_result = select_best_search_result(search_results)
 
-            best_result = select_best_search_result(search_results)
+        uri = fmt_uri(best_result)
 
-            uri = fmt_uri(best_result)
+        if len(markdown.strip()) == 0 or NO_TIPS_PLACEHOLDER.lower() in markdown.lower():
+            markdown = fmt_page_heading(county, state)
 
-            if len(markdown.strip()) == 0 or NO_TIPS_PLACEHOLDER.lower() in markdown.lower():
-                markdown = fmt_page_heading(county, state)
+        markdown = markdown + fmt_entry(title, uri, access_time)
 
-            markdown = markdown + fmt_entry(title, uri, access_time)
-
-            with open(state + "/" + county + ".md", "w") as county_file:
-                county_file.write(markdown)
+        with open(state + "/" + county + ".md", "w") as county_file:
+            county_file.write(markdown)
 
 
 class CountyInfo:
@@ -143,7 +138,7 @@ class CountyInfo:
         self.density = density
 
 
-def select_best_search_result(search_results):
+def get_prioritized_county_list():
     county_info_list = []
     with open('county_list.csv', newline='') as f:
         r = csv.reader(f, delimiter=',')
@@ -153,11 +148,33 @@ def select_best_search_result(search_results):
                 density = calculate_information_density(county_file.read())
                 county_info_list.append(CountyInfo(county, state, density))
 
+    # Naive quartile implementation is safe so long as we have many counties to
+    # work from.
+    county_info_list = sorted(county_info_list, key=lambda x: x.density)
+    quartile_size = len(county_info_list) / 4
+    q1 = county_info_list[quartile_size*0:quartile_size*1]
+    q2 = county_info_list[quartile_size*1:quartile_size*2]
+    q3 = county_info_list[quartile_size*2:quartile_size*3]
+    q4 = county_info_list[quartile_size*3:]
+
+    random.shuffle(q1)
+    random.shuffle(q2)
+    random.shuffle(q3)
+    random.shuffle(q4)
+
+    return [(x.county, x.state) for x in q1+q2+q3+q4]
+
 
 def calculate_information_density(text):
     """estimate information density based on the count of non-whitespace
     characters in the text"""
     return len(["" if c in ['\n', '\r', '\t', ' '] else c for c in text])
+
+
+def select_best_search_result(search_results):
+    # currently implemented to just return the top result, this could be
+    # extended in any number of ways.
+    return search_results[0]
 
 
 def replace_underscores(s):
